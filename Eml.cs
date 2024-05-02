43e55b8b3d44189e68b32becad2af75a2c475e3c
cikal-student <168709114+cikal-student@users.noreply.github.com>
2024-05-02 13:11:55 +0700

    Added Eml.App
    Codes not working for now.

---
+++ Eml.cs
@@ -0,0 +1,114 @@
+﻿/*
+ * Execution Mapping:
+ * Nothing will seek before us, before we affirm.
+ */
+using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using Memory;
+
+namespace Eml
+{
+    class Authentication
+    {
+        readonly Int32 UnixEpoch = (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+        private Dictionary<string, string> EnvVariables = new Dictionary<string, string>();
+        private readonly List<IPAddress> CikalNetworkAddress = new List<IPAddress>
+        {
+            IPAddress.Parse("117.102.85.216"),
+            IPAddress.Parse("117.102.85.223"),
+        };
+        public bool InCikalNetwork(IPAddress address)
+        {
+            Application app = new Application();
+            IPAddress UserAddress = GetPublicIpAddress().Result;
+            try
+            {
+                Byte[] Data = UserAddress.GetAddressBytes();
+            }
+            catch (Exception ex)
+            {
+                app.Print($"Authentication logic error, {ex}");
+            }
+
+            return false;
+        }
+        public Dictionary<string, string> GetEnvironmentTable()
+        {
+            IDictionary envout = Environment.GetEnvironmentVariables();
+            foreach (DictionaryEntry entry in envout)
+            {
+                EnvVariables.Add(entry.Key.ToString(), entry.Value.ToString());
+            }
+            return EnvVariables;
+        }
+        public async Task<IPAddress> GetPublicIpAddress()
+        {
+            Application app = new Application();
+            Authentication auth = new Authentication();
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync("https://api.ipify.org");
+
+                    response.EnsureSuccessStatusCode();
+
+                    string ipAddress = await response.Content.ReadAsStringAsync();
+
+                    return IPAddress.Parse(ipAddress);
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                app.Print($"Error getting public IP address: {e.Message}");
+                return IPAddress.None;
+            }
+        }
+    }
+    class Invasive
+    {
+        private readonly Dictionary<string, int> MemAddress = new Dictionary<string, int>()
+        {
+            {"procopen", 0x00ff },
+        };
+    }
+    class Application
+    {
+        public int LinesPrinted = 0;
+        public string Username { get; set; }
+        public void Print(string message)
+        { 
+            Application app = new Application();
+            app.LinesPrinted = app.LinesPrinted + 1;
+            // Continuation
+            System.Console.WriteLine($"[{app.LinesPrinted}]: {message}"); 
+        }
+        static void Main(string[] args) 
+        {
+            Application app = new Application();
+            Authentication auth = new Authentication();
+            // Continuation
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Win32NT:
+                case PlatformID.Win32S:
+                case PlatformID.Win32Windows:
+                case PlatformID.WinCE:
+                    break;
+
+                case PlatformID.Unix:
+                    break;
+
+                case PlatformID.MacOSX:
+                    break;
+
+                default:
+                    return;
+                    break;
+            }
+        }
+    }
+}
